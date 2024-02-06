d0569fdb4e8fc2ace7ff9bc98cc82ee7b37f0eee
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-02-06 11:59:19 -0300

    Update 6) Conditional.sh

--- 6) Conditional.sh
+++ 6) Conditional.sh
@@ -3,8 +3,7 @@ instrução if-else
 instrução if..elif..else..fi (escada Else If)
 if..then..else..if..then..fi..fi..(Aninhado se)
 declaração de mudança
-
-
+==========================================
 #!/bin/bash
 echo "Digit one value"
 read X
@@ -24,3 +23,36 @@ elif [ $X -lt $Y ];then
 if [ $? -eq 0 ];then
     echo "Operação bem sucedida"
 fi
+
+==========================================
+read a 
+read b
+
+#Check whether they are equal 
+if [ $a == $b ]  # $a != $b 
+then 
+	echo "a is equal to b"
+ 
+else
+	echo "a is not equal to b"
+fi
+==========================================
+#!/bin/bash
+
+echo "Digite um número de 1 a 3:"
+read numero
+
+case $numero in
+  1)
+    echo "Você escolheu um."
+    ;;
+  2)
+    echo "Você escolheu dois."
+    ;;
+  3)
+    echo "Você escolheu três."
+    ;;
+  *)
+    echo "Opção inválida."
+    ;;
+esac
