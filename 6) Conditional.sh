6ee35e891662b1f508961687a95f117663fdd280
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-02-06 08:49:00 -0300

    Update 6) Conditional.sh

--- 6) Conditional.sh
+++ 6) Conditional.sh
@@ -1,1 +1,22 @@
-asd
+declaração if
+instrução if-else
+instrução if..elif..else..fi (escada Else If)
+if..then..else..if..then..fi..fi..(Aninhado se)
+declaração de mudança
+
+
+#!/bin/bash
+echo "Digit one value"
+read X
+echo "Digit other value"
+read Y
+
+if [ $X -eq $Y ];then
+  echo "uh, same values, hm? nice"
+
+elif [ $X -gt $Y ];then
+  echo "This value ${X} is greater than ${Y}"
+
+elif [ $X -lt $Y ];then
+  echo "The value ${Y} is greater than ${X}"
+fi
