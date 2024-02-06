b9863b057ad3d76a921c2561849c40a0648287e2
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-02-06 08:17:48 -0300

    Update 5) Arrays.sh

--- 5) Arrays.sh
+++ 5) Arrays.sh
@@ -1,6 +1,7 @@
 ARRAYNAME=(value1 value2  .... valueN)
 ARRAYNAME=([1]=10 [2]=20 [3]=30)
-Prática:
+
+# Prática:
 =====================================
 #!/bin/bash
 arry=(10 40 50 60)
@@ -8,9 +9,26 @@ for((;c<3;c++))
 do
   echo "read? ${arry[c]}"
 done
-ou..
+
+# Outras formas:
+
+#!/bin/bash
+arry=(10 40 50 60 "hello")
+element_length=${#arry[0]}
+element_length_total=${#arry[@]}
+search=$(echo "${arry[*]}" | grep -c "hello") # conta quantas vezes a string "hello" aparece na saída.
+replace=$(echo "${arry[*]/hello/Jojis}")
+echo "Length is: ${element_length}"
+echo "Length total is: ${element_length_total}"
+echo "Search is : ${search}"
+echo "Replace value: $replace"
 echo "Todos os valores: ${arry[*]}"
+unset arry[2]
 echo "Todos os valores: ${arry[@]}"
+
+=====================================
+echo "Todos os valores: ${arry[*]}" #  imprime todos os elementos do array como uma única string.
+echo "Todos os valores: ${arry[@]}" #imprimir cada elemento do array separadamente. Ele exibe todos os elementos da matriz.
 echo "Valores depois do indice ${arry[4]}: ${arry[@]:4}"
 echo "Valores entre os intervalos de ${arry[1]} e ${arry[4]} : ${arry[*]:1:4}"
 =====================================
@@ -20,6 +38,8 @@ echo "Elemento 2: ${my_array[1]}"
 echo "Elemento 3: ${my_array[2]}"
 echo "Elemento 4: ${my_array[3]}"
 echo "Elemento 5: ${my_array[4]}"
+
+# Mudança de valores na posição: 
 
 Se deseja mudar algum valor nas posições é, simples:
 my_array[2]=99
