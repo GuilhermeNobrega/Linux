9b7bf1c2f21c551717f41b0ed83105ea71ff17c5
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-02-09 09:12:16 -0300

    Update 8) Functions.sh

--- 8) Functions.sh
+++ 8) Functions.sh
@@ -1,5 +1,5 @@
-======================================================================
-Funções são fáceis e agilizam processos longos:
+#======================================================================
+#Funções são fáceis e agilizam processos longos:
 
 #!/bin/bash
 a=1
@@ -22,3 +22,16 @@ my_variable_value="Hello World"
 
 # Chamando a função e passando a variável como argumento
 example_function "$my_variable_value"
+#======================================================================
+
+# Passing arguments before running
+
+receber(){
+    echo "$0"
+    var1=$1
+    var2=$2
+    echo "Valores passados: $var1 e $var2"
+}
+
+receber $1 $2
+
