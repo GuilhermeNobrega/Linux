5c950d0615ec3e4426ce8061f37c6e561668c9a8
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-04-10 10:28:52 -0300

    Create LinuxImportant.sh

---
+++ LinuxImportant.sh
@@ -0,0 +1,11 @@
+# Para buscar um pacote, faça:
+
+apt-cache search nome_pacote
+apt search nome_pacotee
+apt --help
+#Cache serve para apresentar todas as informações do pacote e fazer sua busca
+
+# Para remover um pacote, faça:
+
+apt-get remove nome_pacote
+apt remove nome_pacote
