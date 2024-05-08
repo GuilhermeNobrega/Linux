e7b0fcf4c950b9ecd784465cb849748783cc4390
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-05-08 10:23:55 -0300

    Update 13) RegularExpressions(RegEx).sh

--- 13) RegularExpressions(RegEx).sh
+++ 13) RegularExpressions(RegEx).sh
@@ -3,6 +3,7 @@
 #Test Regular Expressions (RegEx) on Linux
 #Using “^” (caret) to match the beginning of the string
 # Basicamente serve para procurar início de linhas que começam com a expressão desejada
+
 finder=$(cat leitura.sh | grep ^e)
 
 echo "$finder"
