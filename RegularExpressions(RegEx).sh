c16cfe450ddf2dd9e8f662fd9c28dbf72253bdc9
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-05-08 10:07:13 -0300

    Create RegularExpressions(RegEx).sh

---
+++ RegularExpressions(RegEx).sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Test Regular Expressions (RegEx) on Linux
+#Using “^” (caret) to match the beginning of the string
+# Basicamente serve para procurar início de linhas que começam com a expressão desejada
+finder=$(cat leitura.sh | grep ^e)
+
+echo "$finder"
+
