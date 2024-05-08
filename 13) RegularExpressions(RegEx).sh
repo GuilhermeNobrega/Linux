f2df9dae89b7777529ebb009556c0086a74b5194
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-05-08 11:48:52 -0300

    Update 13) RegularExpressions(RegEx).sh

--- 13) RegularExpressions(RegEx).sh
+++ 13) RegularExpressions(RegEx).sh
@@ -8,3 +8,18 @@ finder=$(cat leitura.sh | grep ^e)
 
 echo "$finder"
 
+#============================================================================================
+
+# It is called a wild card character, It matches any one character other than the new line.
+# O ponto pode ser utilizado para representar tudo ou apenas para encontrar palavras que contenham um ou mais caracteres após um prefixo específico.
+
+grep ba. arquivo.txt #(acha palavras que tenham ba seguidas de qualquer caractere)
+grep '.a' arquivo.txt # Significa qualquer sequência de dois caracteres onde o segundo caractere é a letra a
+echo "---"
+grep 'a.' arquivo.txt # Significa sequência que de dois caracteres onde o primeiro precisa ser a letra a
+
+
+#============================================================================================
+#to match the ending of the string
+
+cat arquivo.txt | grep 'e$'
