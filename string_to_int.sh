db160d2e3cbf7df0520700ba4f734ad387d71ddd
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-05-30 16:40:45 -0300

    Update string_to_int.sh

--- string_to_int.sh
+++ string_to_int.sh
@@ -4,6 +4,9 @@ string_number="42+Hello"
 
 # Convert string to an integer using arithmetic expansion
 integer_result=$((string_number))
+integer_result=$(expr "$string_number" + 0)
+let "integer_result=$string_number"
+integer_result=$(echo "$string_number" | bc)
 
 echo "Original String: $string_number"
 echo "Converted Integer: $integer_result"
